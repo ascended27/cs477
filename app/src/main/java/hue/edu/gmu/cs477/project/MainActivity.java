57936431b11e3637140abed393d705a2b6855031
ascended27 <ascended27@gmail.com>
2017-10-02 16:55:22 -0400

    Init

---
+++ app/src/main/java/hue/edu/gmu/cs477/project/MainActivity.java
@@ -0,0 +1,13 @@
+package hue.edu.gmu.cs477.project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
