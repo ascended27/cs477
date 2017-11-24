656fbc8ca5f121ee774ec9dd149284543b81bad3
Hansol Shin <hansol.shin@icloud.com>
2017-11-23 19:39:04 -0500

    Added Tarsos Inner class
    Added Tarsos Inner class and added potential dependency jars.

--- app/src/main/java/hue/edu/gmu/cs477/project/MainActivity.java
+++ app/src/main/java/hue/edu/gmu/cs477/project/MainActivity.java
@@ -15,55 +15,80 @@ import be.tarsos.dsp.pitch.PitchProcessor;
 
 public class MainActivity extends AppCompatActivity {
 
+    final int RC_PERM = 200;
     String[] perms = {"android.permission.RECORD_AUDIO"};
-
-    int permsRequestCode = 200;
+    Tarsos tarsos;
 
     @Override
     public void onRequestPermissionsResult(int permsRequestCode, String[] permissions, int[] grantResults){
-
         switch(permsRequestCode){
-
             case 200:
-
                 boolean audioAccepted = grantResults[0]== PackageManager.PERMISSION_GRANTED;
-
+                tarsos = new Tarsos();
                 break;
-
+            default:
+                return;
         }
-
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initialize();
+    }
+
+    /**
+     * Initializes the app when the app is first created
+     */
+    private void initialize() {
+        requestPermissions(perms, RC_PERM);
         setContentView(R.layout.activity_main);
+    }
 
-        requestPermissions(perms, permsRequestCode);
+    /**
+     * Tarsos wrapper
+     */
+    private class Tarsos {
 
-        AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050,1024,0);
+        AudioDispatcher dispatcher;
+        PitchDetectionHandler handler;
+        AudioProcessor processor;
 
-        PitchDetectionHandler pdh = new PitchDetectionHandler() {
-            @Override
-            public void handlePitch(PitchDetectionResult result, AudioEvent e) {
-                try {
-                    final float pitchInHz = result.getPitch();
-                    if(pitchInHz != -1) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                TextView text = (TextView) findViewById(R.id.textView1);
-                                text.setText("" + pitchInHz);
-                            }
-                        });
+        Tarsos() {
+            dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0);
+
+            handler = new PitchDetectionHandler() {
+                @Override
+                public void handlePitch(PitchDetectionResult result, AudioEvent e) {
+                    try {
+                        final float pitchInHz = result.getPitch();
+                        if (pitchInHz != -1) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    TextView text = (TextView) findViewById(R.id.textView1);
+                                    text.setText("" + pitchInHz);
+                                }
+                            });
+                        }
+                    } catch (Exception ex) {
                     }
-                } catch(Exception ex){
-
                 }
-            }
-        };
-        AudioProcessor p = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.AMDF, 22050, 1024, pdh);
-        dispatcher.addAudioProcessor(p);
-        new Thread(dispatcher,"Audio Dispatcher").start();
+            };
+
+            processor = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.AMDF, 22050, 1024, handler);
+        }
+
+        private void attachProcessor() {
+            dispatcher.addAudioProcessor(processor);
+        }
+
+        private void detachProcessor() {
+            dispatcher.removeAudioProcessor(processor);
+        }
+
+        private void dispatch() {
+            new Thread(dispatcher, "Audio Dispatcher").start();
+        }
     }
 }
